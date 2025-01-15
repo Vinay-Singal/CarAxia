2af024bf91eeae1cec1f08b2ad12a887aae43026
VINAY SINGAL <vinaysingal123ghn@gmail.com>
2025-01-15 19:53:52 +0530

    Update search.php

--- carrental/search.php
+++ carrental/search.php
@@ -11,7 +11,7 @@ error_reporting(0);
 <title>Car Rental Portal | Car Listing</title>
 <!--Bootstrap -->
 <link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css">
-<!--Custome Style -->
+<!--Custome Style's -->
 <link rel="stylesheet" href="assets/css/style.css" type="text/css">
 <!--OWL Carousel slider-->
 <link rel="stylesheet" href="assets/css/owl.carousel.css" type="text/css">
