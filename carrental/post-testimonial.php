b348803445b12e5660d1645c942e145c822cb5c6
VINAY SINGAL <vinaysingal123ghn@gmail.com>
2025-01-27 12:10:47 +0530

    Update post-testimonial.php

--- carrental/post-testimonial.php
+++ carrental/post-testimonial.php
@@ -34,7 +34,7 @@ $error="Something went wrong. Please try again";
 <head>
 
 <title>Car Rental Portal |Post testimonial</title>
-<!--Bootstrap -->
+<!--Bootstraps -->
 <link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css">
 <!--Custome Style -->
 <link rel="stylesheet" href="assets/css/style.css" type="text/css">
